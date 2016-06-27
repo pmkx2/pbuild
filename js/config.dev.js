05b3dc5cedd4b45b668ac43197b383e6cd187282
LuoYongjiang <185832959@qq.com>
2016-06-27 09:25:45 +0800

    新增query的原始库

--- js/config.dev.js
+++ js/config.dev.js
@@ -7,6 +7,7 @@ require.config({
 "bootstrap":"vendor/bootstrap/bootstrap",
 "jquery/1.8.3":"vendor/jquery/1.8.3/jquery.min",
 "jquery/2.1.4":"vendor/jquery/2.1.4/jquery.min",
+"jquery":"vendor/jquery/jquery",
 "require":"vendor/require/require_og"
 },
 		    "shim": {
@@ -17,9 +18,10 @@ require.config({
 "jstest/copy":"af6fc1",
 "jstest/data":"556ffc",
 "bootstrap":"4becdc",
+"require":"e1484b",
 "jquery/1.8.3":"3576a6",
 "jquery/2.1.4":"b0dc11",
-"require":"e1484b"
+"jquery":"c07f22"
 },
 		    "packages": []
 		});
